3226293993761d7dfdb45800a27c29d47abc1486
yapondjian <junioryapondjian@gmail.com>
2024-11-26 15:16:49 -0300

    feat: add contador

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -18,45 +18,46 @@ import * as $$$$$$5 from "./sections/Content/Faq.tsx";
 import * as $$$$$$6 from "./sections/Content/Logos.tsx";
 import * as $$$$$$7 from "./sections/Content/Testimonials.tsx";
 import * as $$$$$$8 from "./sections/Content/TextSeo.tsx";
-import * as $$$$$$9 from "./sections/Footer/Footer.tsx";
-import * as $$$$$$10 from "./sections/Gallery.tsx";
-import * as $$$$$$11 from "./sections/Header/Header.tsx";
-import * as $$$$$$12 from "./sections/Images/BannerCta.tsx";
-import * as $$$$$$13 from "./sections/Images/BannerGrid.tsx";
-import * as $$$$$$14 from "./sections/Images/Carousel.tsx";
-import * as $$$$$$15 from "./sections/Images/Collections.tsx";
-import * as $$$$$$16 from "./sections/Images/CuponsList.tsx";
-import * as $$$$$$17 from "./sections/Images/ImageGallery.tsx";
-import * as $$$$$$18 from "./sections/Images/ImageTextButtonComponent.tsx";
-import * as $$$$$$19 from "./sections/Images/Infocard.tsx";
-import * as $$$$$$20 from "./sections/Images/ShoppableBanner.tsx";
-import * as $$$$$$21 from "./sections/Institucional/Institucional.tsx";
-import * as $$$$$$22 from "./sections/Institucional/OurStores.tsx";
-import * as $$$$$$23 from "./sections/Institucional/Reseller.tsx";
-import * as $$$$$$24 from "./sections/Layout/Container.tsx";
-import * as $$$$$$25 from "./sections/Layout/Flex.tsx";
-import * as $$$$$$26 from "./sections/Layout/Grid.tsx";
-import * as $$$$$$27 from "./sections/Layout/GridItem.tsx";
-import * as $$$$$$28 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$29 from "./sections/Links/Shortcuts.tsx";
-import * as $$$$$$30 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$31 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$32 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$33 from "./sections/Product/ImageGalleryFrontBack.tsx";
-import * as $$$$$$34 from "./sections/Product/ImageGallerySlider.tsx";
-import * as $$$$$$35 from "./sections/Product/NotFound.tsx";
-import * as $$$$$$36 from "./sections/Product/NotFoundChallenge.tsx";
-import * as $$$$$$37 from "./sections/Product/ProductInfo.tsx";
-import * as $$$$$$38 from "./sections/Product/ProductMain.tsx";
-import * as $$$$$$39 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$40 from "./sections/Product/ProductShelfTabbed.tsx";
-import * as $$$$$$41 from "./sections/Product/ProductShelfWithBanner.tsx";
-import * as $$$$$$42 from "./sections/Product/Regionalization.tsx";
-import * as $$$$$$43 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$44 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$45 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$46 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$47 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$9 from "./sections/Content/TimerCount.tsx";
+import * as $$$$$$10 from "./sections/Footer/Footer.tsx";
+import * as $$$$$$11 from "./sections/Gallery.tsx";
+import * as $$$$$$12 from "./sections/Header/Header.tsx";
+import * as $$$$$$13 from "./sections/Images/BannerCta.tsx";
+import * as $$$$$$14 from "./sections/Images/BannerGrid.tsx";
+import * as $$$$$$15 from "./sections/Images/Carousel.tsx";
+import * as $$$$$$16 from "./sections/Images/Collections.tsx";
+import * as $$$$$$17 from "./sections/Images/CuponsList.tsx";
+import * as $$$$$$18 from "./sections/Images/ImageGallery.tsx";
+import * as $$$$$$19 from "./sections/Images/ImageTextButtonComponent.tsx";
+import * as $$$$$$20 from "./sections/Images/Infocard.tsx";
+import * as $$$$$$21 from "./sections/Images/ShoppableBanner.tsx";
+import * as $$$$$$22 from "./sections/Institucional/Institucional.tsx";
+import * as $$$$$$23 from "./sections/Institucional/OurStores.tsx";
+import * as $$$$$$24 from "./sections/Institucional/Reseller.tsx";
+import * as $$$$$$25 from "./sections/Layout/Container.tsx";
+import * as $$$$$$26 from "./sections/Layout/Flex.tsx";
+import * as $$$$$$27 from "./sections/Layout/Grid.tsx";
+import * as $$$$$$28 from "./sections/Layout/GridItem.tsx";
+import * as $$$$$$29 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$30 from "./sections/Links/Shortcuts.tsx";
+import * as $$$$$$31 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$32 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$33 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$34 from "./sections/Product/ImageGalleryFrontBack.tsx";
+import * as $$$$$$35 from "./sections/Product/ImageGallerySlider.tsx";
+import * as $$$$$$36 from "./sections/Product/NotFound.tsx";
+import * as $$$$$$37 from "./sections/Product/NotFoundChallenge.tsx";
+import * as $$$$$$38 from "./sections/Product/ProductInfo.tsx";
+import * as $$$$$$39 from "./sections/Product/ProductMain.tsx";
+import * as $$$$$$40 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$41 from "./sections/Product/ProductShelfTabbed.tsx";
+import * as $$$$$$42 from "./sections/Product/ProductShelfWithBanner.tsx";
+import * as $$$$$$43 from "./sections/Product/Regionalization.tsx";
+import * as $$$$$$44 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$45 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$46 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$47 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$48 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "loaders": {
@@ -75,48 +76,49 @@ const manifest = {
     "deco-sites/comfortflex/sections/Content/Logos.tsx": $$$$$$6,
     "deco-sites/comfortflex/sections/Content/Testimonials.tsx": $$$$$$7,
     "deco-sites/comfortflex/sections/Content/TextSeo.tsx": $$$$$$8,
-    "deco-sites/comfortflex/sections/Footer/Footer.tsx": $$$$$$9,
-    "deco-sites/comfortflex/sections/Gallery.tsx": $$$$$$10,
-    "deco-sites/comfortflex/sections/Header/Header.tsx": $$$$$$11,
-    "deco-sites/comfortflex/sections/Images/BannerCta.tsx": $$$$$$12,
-    "deco-sites/comfortflex/sections/Images/BannerGrid.tsx": $$$$$$13,
-    "deco-sites/comfortflex/sections/Images/Carousel.tsx": $$$$$$14,
-    "deco-sites/comfortflex/sections/Images/Collections.tsx": $$$$$$15,
-    "deco-sites/comfortflex/sections/Images/CuponsList.tsx": $$$$$$16,
-    "deco-sites/comfortflex/sections/Images/ImageGallery.tsx": $$$$$$17,
+    "deco-sites/comfortflex/sections/Content/TimerCount.tsx": $$$$$$9,
+    "deco-sites/comfortflex/sections/Footer/Footer.tsx": $$$$$$10,
+    "deco-sites/comfortflex/sections/Gallery.tsx": $$$$$$11,
+    "deco-sites/comfortflex/sections/Header/Header.tsx": $$$$$$12,
+    "deco-sites/comfortflex/sections/Images/BannerCta.tsx": $$$$$$13,
+    "deco-sites/comfortflex/sections/Images/BannerGrid.tsx": $$$$$$14,
+    "deco-sites/comfortflex/sections/Images/Carousel.tsx": $$$$$$15,
+    "deco-sites/comfortflex/sections/Images/Collections.tsx": $$$$$$16,
+    "deco-sites/comfortflex/sections/Images/CuponsList.tsx": $$$$$$17,
+    "deco-sites/comfortflex/sections/Images/ImageGallery.tsx": $$$$$$18,
     "deco-sites/comfortflex/sections/Images/ImageTextButtonComponent.tsx":
-      $$$$$$18,
-    "deco-sites/comfortflex/sections/Images/Infocard.tsx": $$$$$$19,
-    "deco-sites/comfortflex/sections/Images/ShoppableBanner.tsx": $$$$$$20,
-    "deco-sites/comfortflex/sections/Institucional/Institucional.tsx": $$$$$$21,
-    "deco-sites/comfortflex/sections/Institucional/OurStores.tsx": $$$$$$22,
-    "deco-sites/comfortflex/sections/Institucional/Reseller.tsx": $$$$$$23,
-    "deco-sites/comfortflex/sections/Layout/Container.tsx": $$$$$$24,
-    "deco-sites/comfortflex/sections/Layout/Flex.tsx": $$$$$$25,
-    "deco-sites/comfortflex/sections/Layout/Grid.tsx": $$$$$$26,
-    "deco-sites/comfortflex/sections/Layout/GridItem.tsx": $$$$$$27,
-    "deco-sites/comfortflex/sections/Links/LinkTree.tsx": $$$$$$28,
-    "deco-sites/comfortflex/sections/Links/Shortcuts.tsx": $$$$$$29,
-    "deco-sites/comfortflex/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$30,
-    "deco-sites/comfortflex/sections/Miscellaneous/CookieConsent.tsx": $$$$$$31,
-    "deco-sites/comfortflex/sections/Newsletter/Newsletter.tsx": $$$$$$32,
+      $$$$$$19,
+    "deco-sites/comfortflex/sections/Images/Infocard.tsx": $$$$$$20,
+    "deco-sites/comfortflex/sections/Images/ShoppableBanner.tsx": $$$$$$21,
+    "deco-sites/comfortflex/sections/Institucional/Institucional.tsx": $$$$$$22,
+    "deco-sites/comfortflex/sections/Institucional/OurStores.tsx": $$$$$$23,
+    "deco-sites/comfortflex/sections/Institucional/Reseller.tsx": $$$$$$24,
+    "deco-sites/comfortflex/sections/Layout/Container.tsx": $$$$$$25,
+    "deco-sites/comfortflex/sections/Layout/Flex.tsx": $$$$$$26,
+    "deco-sites/comfortflex/sections/Layout/Grid.tsx": $$$$$$27,
+    "deco-sites/comfortflex/sections/Layout/GridItem.tsx": $$$$$$28,
+    "deco-sites/comfortflex/sections/Links/LinkTree.tsx": $$$$$$29,
+    "deco-sites/comfortflex/sections/Links/Shortcuts.tsx": $$$$$$30,
+    "deco-sites/comfortflex/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$31,
+    "deco-sites/comfortflex/sections/Miscellaneous/CookieConsent.tsx": $$$$$$32,
+    "deco-sites/comfortflex/sections/Newsletter/Newsletter.tsx": $$$$$$33,
     "deco-sites/comfortflex/sections/Product/ImageGalleryFrontBack.tsx":
-      $$$$$$33,
-    "deco-sites/comfortflex/sections/Product/ImageGallerySlider.tsx": $$$$$$34,
-    "deco-sites/comfortflex/sections/Product/NotFound.tsx": $$$$$$35,
-    "deco-sites/comfortflex/sections/Product/NotFoundChallenge.tsx": $$$$$$36,
-    "deco-sites/comfortflex/sections/Product/ProductInfo.tsx": $$$$$$37,
-    "deco-sites/comfortflex/sections/Product/ProductMain.tsx": $$$$$$38,
-    "deco-sites/comfortflex/sections/Product/ProductShelf.tsx": $$$$$$39,
-    "deco-sites/comfortflex/sections/Product/ProductShelfTabbed.tsx": $$$$$$40,
+      $$$$$$34,
+    "deco-sites/comfortflex/sections/Product/ImageGallerySlider.tsx": $$$$$$35,
+    "deco-sites/comfortflex/sections/Product/NotFound.tsx": $$$$$$36,
+    "deco-sites/comfortflex/sections/Product/NotFoundChallenge.tsx": $$$$$$37,
+    "deco-sites/comfortflex/sections/Product/ProductInfo.tsx": $$$$$$38,
+    "deco-sites/comfortflex/sections/Product/ProductMain.tsx": $$$$$$39,
+    "deco-sites/comfortflex/sections/Product/ProductShelf.tsx": $$$$$$40,
+    "deco-sites/comfortflex/sections/Product/ProductShelfTabbed.tsx": $$$$$$41,
     "deco-sites/comfortflex/sections/Product/ProductShelfWithBanner.tsx":
-      $$$$$$41,
-    "deco-sites/comfortflex/sections/Product/Regionalization.tsx": $$$$$$42,
-    "deco-sites/comfortflex/sections/Product/SearchResult.tsx": $$$$$$43,
-    "deco-sites/comfortflex/sections/Product/Wishlist.tsx": $$$$$$44,
-    "deco-sites/comfortflex/sections/Social/InstagramPosts.tsx": $$$$$$45,
-    "deco-sites/comfortflex/sections/Social/WhatsApp.tsx": $$$$$$46,
-    "deco-sites/comfortflex/sections/Theme/Theme.tsx": $$$$$$47,
+      $$$$$$42,
+    "deco-sites/comfortflex/sections/Product/Regionalization.tsx": $$$$$$43,
+    "deco-sites/comfortflex/sections/Product/SearchResult.tsx": $$$$$$44,
+    "deco-sites/comfortflex/sections/Product/Wishlist.tsx": $$$$$$45,
+    "deco-sites/comfortflex/sections/Social/InstagramPosts.tsx": $$$$$$46,
+    "deco-sites/comfortflex/sections/Social/WhatsApp.tsx": $$$$$$47,
+    "deco-sites/comfortflex/sections/Theme/Theme.tsx": $$$$$$48,
   },
   "actions": {
     "deco-sites/comfortflex/actions/newsletter/record.ts": $$$$$$$$$0,
---
+++ sections/Content/TimerCount.tsx
@@ -0,0 +1,145 @@
+import { useId } from "$store/sdk/useId.ts";
+import type { HTMLWidget } from "apps/admin/widgets.ts";
+
+export interface Props {
+  /**
+   * @title Text
+   * @default Time left for a campaign to end wth a link
+   */
+  text?: HTMLWidget;
+
+  /**
+   * @title Expires at date
+   * @format datetime
+   */
+  expiresAt?: string;
+
+  labels?: {
+    /**
+     * @title Text to show when expired
+     */
+    expired?: string;
+    hours?: string;
+    minutes?: string;
+    seconds?: string;
+  };
+}
+
+const snippet = (expiresAt: string, rootId: string) => {
+  const expirationDate = new Date(expiresAt).getTime();
+
+  const getDelta = () => {
+    const delta = expirationDate - new Date().getTime();
+
+    const days = Math.floor(delta / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (delta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+    );
+    const minutes = Math.floor((delta % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((delta % (1000 * 60)) / 1000);
+    const totalHours = (days * 24) + hours;
+
+    return {
+      hours: Math.min(totalHours, 99),
+      minutes,
+      seconds,
+    };
+  };
+
+  const setValue = (id: string, value: number) => {
+    const elem = document.getElementById(id);
+
+    if (!elem) return;
+
+    elem.style.setProperty("--value", value.toString());
+  };
+
+  const start = () =>
+    setInterval(() => {
+      const { hours, minutes, seconds } = getDelta();
+      const isExpired = hours + minutes + seconds < 0;
+
+      if (isExpired) {
+        const expired = document.getElementById(`${rootId}::expired`);
+        const counter = document.getElementById(`${rootId}::counter`);
+
+        expired && expired.classList.remove("hidden");
+        counter && counter.classList.add("hidden");
+      } else {
+        setValue(`${rootId}::hours`, hours);
+        setValue(`${rootId}::minutes`, minutes);
+        setValue(`${rootId}::seconds`, seconds);
+      }
+    }, 1_000);
+
+  document.readyState === "complete"
+    ? start()
+    : addEventListener("load", start);
+};
+
+function TimerCount({
+  expiresAt = `${new Date()}`,
+  labels,
+  text = "Oferta relâmpago",
+}: Props) {
+  const id = useId();
+
+  return (
+    <>
+      <div id={id} class="w-full">
+        <div class="mx-auto flex flex-col lg:flex-row lg:items-center lg:justify-center p-4 gap-4 bg-black" style="max-width: 570px; background-color: #26152F;">
+          
+              <div
+                class="text-center lg:text-2xl lg:text-left lg:max-w-lg uppercase font-bold text-white"
+                dangerouslySetInnerHTML={{ __html: text }}
+              >
+              </div>
+          <div
+            id={`${id}::expired`}
+            class="hidden text-sm text-center lg:text-xl lg:text-left lg:max-w-lg"
+          >
+            {labels?.expired || "Expired!"}
+          </div>
+          <div class="flex gap-8 lg:gap-16 items-center justify-center lg:justify-normal">
+            <div id={`${id}::counter`}>
+              <div class="grid grid-flow-col gap-3 text-center auto-cols-max items-center text-white">
+                <div class="flex flex-col text-xs lg:text-sm uppercase text-center">
+                  <span class="countdown font-normal text-xl lg:text-2xl  justify-center">
+                    <span id={`${id}::hours`} />
+                  </span>
+                  {labels?.hours || ""}
+                </div>
+                <div>
+                  :
+                </div>
+                <div class="flex flex-col text-xs lg:text-sm uppercase text-center">
+                  <span class="countdown font-normal text-xl lg:text-2xl  justify-center">
+                    <span id={`${id}::minutes`} />
+                  </span>
+                  {labels?.minutes || ""}
+                </div>
+                <div>
+                  :
+                </div>
+                <div class="flex flex-col text-xs lg:text-sm uppercase text-center">
+                  <span class="countdown font-normal text-xl lg:text-2xl  justify-center">
+                    <span id={`${id}::seconds`} />
+                  </span>
+                  {labels?.seconds || ""}
+                </div>
+              </div>
+            </div>            
+          </div>
+        </div>
+      </div>
+      <script
+        type="module"
+        dangerouslySetInnerHTML={{
+          __html: `(${snippet})("${expiresAt}", "${id}");`,
+        }}
+      />
+    </>
+  );
+}
+
+export default TimerCount;
