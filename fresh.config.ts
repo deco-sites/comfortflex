5e36a57fb6010bce2841a8bf40fda9f64527c777
Martin Adam <martin.viana99@gmail.com>
2025-01-15 21:54:51 -0300

    update imports

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,16 +1,22 @@
 import { defineConfig } from "$fresh/server.ts";
-import plugins from "https://denopkg.com/deco-sites/std@1.24.1/plugins/mod.ts";
+import { plugins } from "deco/plugins/deco.ts";
 import partytownPlugin from "partytown/mod.ts";
 import decoManifest from "./manifest.gen.ts";
-import tailwind from "./tailwind.config.ts";
 
 export default defineConfig({
   plugins: [
     ...plugins({
       manifest: decoManifest,
       // deno-lint-ignore no-explicit-any
-      tailwind: tailwind as any,
+      
     }),
     partytownPlugin(),
   ],
+  render: (ctx, render) => {
+    ctx.lang = "pt-BR";
+    render();
+  },
+  build: {
+    target: ["es2020", "safari12"],
+  },
 });
