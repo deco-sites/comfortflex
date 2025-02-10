c273125a0de2b9c96d6734e88a4f02537463db15
PedroLucasAraujo <pedro.lucas@almahcomunicacao.com.br>
2025-02-10 13:45:04 -0300

    fix: remove borda preta da newsletter

--- components/footer/Newsletter.tsx
+++ components/footer/Newsletter.tsx
@@ -76,7 +76,7 @@ function Newsletter(
                 />
                 <button
                   type="submit"
-                  class="btn disabled:loading w-full lg:w-28 mt-4 lg:mt-0 rounded-3xl bg-white"
+                  class="btn disabled:loading w-full lg:w-28 mt-4 lg:mt-0 rounded-3xl bg-white border-none"
                   disabled={loading}
                 >
                   ASSINAR!
