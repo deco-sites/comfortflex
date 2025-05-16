b29017ff50d0f1bf8e7304fafcd6750e8feb9d10
PedroLucasAraujo <pedro.lucas@almahcomunicacao.com.br>
2025-05-16 12:02:20 -0300

    fix: size table fix

--- components/product/MeasurementChart.tsx
+++ components/product/MeasurementChart.tsx
@@ -1,13 +1,18 @@
+
+import Image from "apps/website/components/Image.tsx";
+import {ImageWidget} from "apps/admin/widgets.ts";
+
 import { signal } from "@preact/signals";
 import Modal from "$store/components/ui/Modal.tsx";
 
 const openModal = signal(false);
+const activeTab = signal<"chart" | "guide">("chart");
 
 interface Props {
-  sizeTable: ImageWidget;
-  sizeTableHowTo: ImageWidget;
+  sizeTable: ImageWidget
+  sizeTableHowTo: ImageWidget
 }
-export default function MeasurementChart({ sizeTable, sizeTableHowTo }: Props) {
+export default function MeasurementChart({sizeTable, sizeTableHowTo}: Props) {
   const value = openModal.value;
 
   const openModalHandler = () => openModal.value = true;
@@ -17,7 +22,7 @@ export default function MeasurementChart({ sizeTable, sizeTableHowTo }: Props) {
   return (
     <>
       <button
-        class="text-sm text-black font-semibold underline"
+        class="text-sm text-black font-semibold underline max-lg:mt-2"
         onClick={openModalHandler}
       >
         Ver tabela de medidas
@@ -31,27 +36,19 @@ export default function MeasurementChart({ sizeTable, sizeTableHowTo }: Props) {
           <div class="flex flex-col gap-3 w-[calc(100%-24px)] max-w-3xl max-h-screen overflow-y-auto absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] bg-white p-3 pb-5 rounded-sm">
             <div class="flex justify-end items-center">
               <button onClick={() => closeModalHandler()}>
-                <Icon id="XMark" size={24} strokeWidth={2} />
+                X
               </button>
             </div>
             {/* Cabeçalho com abas */}
-            <div class="flex border-b border-[#070707] text-sm/4">
+            <div class="flex border-b border-[#BC81FF] text-sm/4">
               <button
-                class={`flex-1 px-3 py-2 font-light rounded-t-lg ${
-                  activeTab.value === "chart"
-                    ? "bg-[#070707] text-white"
-                    : "text-[#353535]"
-                }`}
+                class={`flex-1 px-3 py-2 font-light rounded-t-lg ${activeTab.value === "chart" ? "bg-brand text-white" : "text-[#353535]"}`}
                 onClick={() => switchTab("chart")}
               >
                 TABELA DE MEDIDAS
               </button>
               <button
-                class={`flex-1 px-3 py-2 font-light rounded-t-lg ${
-                  activeTab.value === "guide"
-                    ? "bg-[#070707] text-white"
-                    : "text-[#353535]"
-                }`}
+                class={`flex-1 px-3 py-2 font-light rounded-t-lg ${activeTab.value === "guide" ? "bg-brand text-white" : "text-[#353535]"}`}
                 onClick={() => switchTab("guide")}
               >
                 COMO SE MEDIR
@@ -60,30 +57,24 @@ export default function MeasurementChart({ sizeTable, sizeTableHowTo }: Props) {
 
             {/* Conteúdo das abas */}
             <div class="flex items-center justify-center">
-              {activeTab.value === "chart"
-                ? (
-                  <div>
+              {activeTab.value === "chart" ? (
+                <div>
+                  <Image
+                      src={sizeTable ? sizeTable.src : 'https://assets.decocache.com/comfortflex/5f4d1b8d-f276-4cda-b20c-6310c87648a3/size-table-comfort.png'}
+                      alt={'Medição Pé'}
+                      class="h-full w-auto" />
+                </div>
+              ) : (
+                <div>
                     <Image
-                      src={sizeTable
-                        ? sizeTable.src
-                        : "https://assets.decocache.com/comfortflex/5f4d1b8d-f276-4cda-b20c-6310c87648a3/size-table-comfort.png"}
-                      alt={"Medição Pé"}
-                      class="h-full w-auto"
-                    />
-                  </div>
-                )
-                : (
-                  <div>
-                    <Image
-                      src={sizeTableHowTo
-                        ? sizeTableHowTo.src
-                        : "https://assets.decocache.com/comfortflex/52aa5177-e06a-46e0-a59f-b3e259b11d68/size-table-guide-comfort.png"}
-                      alt={"Medição Pé"}
-                      class="h-full w-auto"
-                    />
-                  </div>
-                )}
+                      src={sizeTableHowTo ? sizeTableHowTo.src : 'https://assets.decocache.com/comfortflex/52aa5177-e06a-46e0-a59f-b3e259b11d68/size-table-guide-comfort.png'}
+                      alt={'Medição Pé'}
+                      class="h-full w-auto" />
+                </div>
+                
+              )}
             </div>
+
           </div>
         </Modal>
       </div>
